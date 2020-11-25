81c4c116eec5edd73a7735579f8ebc4622239dac
Antony Merle <antony.merle@univ-pau.fr>
2020-11-25 18:16:42 +0100

    postgresql

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,14 @@
-interface User {
-  name: string,
-  age: number
+// 16:32 https://www.youtube.com/watch?v=I6ypD7qv3Z8&t=611s
+import {MikroORM } from "@mikro-orm/core"
+import { __prod__ } from "./constants";
+
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    dbName: 'lireddit',
+    type: 'postgresql',
+    user: '',
+    password: '',
+    debug: !__prod__
+  });
 }
-
-const user: User = {
-  name: "Antony",
-  age: 38
-}
-
-class Utilisateur {
-  nom: string;
-  age: number;
-
-  constructor(nom: string, age: number){
-    this.nom = nom;
-    this.age = age;
-  }
-}
-
-const Antony = new Utilisateur("Antony", 38);
-
-const tableau: number[] = [1, 2, 3, 4, 5, 10];
-
-console.log(tableau.reduce((sum: number, current: number) => sum + current, 0));
-
-
-enum Directions {
-  Haut,
-  Bas,
-  Gauche,
-  Droit
-}
-
-const enBas = Directions.Bas;
-
-
